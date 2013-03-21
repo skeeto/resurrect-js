0f90cdd9ac6c8a842bf2607ff7747513efe178c2
Christopher Wellons <mosquitopsu@gmail.com>
2013-03-20 22:39:22 -0400

    Write up documentation.

--- resurrect.js
+++ resurrect.js
@@ -1,14 +1,58 @@
 /**
- *   Resurrect preserves object behavior (prototypes) and reference
- * circularity with a special JSON encoding. Date objects are also
- * properly preserved.
+ * ResurrectJS preserves object behavior (prototypes) and reference
+ * circularity with a special JSON encoding. Unlike regular JSON, Date
+ * objects and undefined are also properly preserved.
+ *
+ * ## Examples
+ *
+ * function Foo() {}
+ * Foo.prototype.greet = function() { return "hello"; };
+ *
+ * // Behavior is preserved:
+ * var necromancer = new Resurrect();
+ * var json = necromancer.stringify(new Foo());
+ * var foo = necromancer.resurrect(json);
+ * foo.greet();  // => "hello"
+ *
+ * // References to the same object are preserved:
+ * json = necromancer.stringify([foo, foo]);
+ * var array = necromancer.resurrect(json);
+ * array[0] === array[1];  // => true
+ * array[1].greet();  // => "hello"
+ *
+ * // Dates are restored properly
+ * json = necromancer.stringify(new Date());
+ * var date = necromancer.resurrect(json);
+ * Object.prototype.toString.call(date);  // => "[object Date]"
+ *
+ * ## Options
+ *
+ * Options are provided to the constructor as an object with these
+ * properties:
+ *
+ *   prefix ('#'): A prefix string used for temporary properties added
+ *     to objects during serialization and deserialization. It is
+ *     important that you don't use any properties beginning with this
+ *     string. This option must be consistent between both
+ *     serialization and deserialization.
+ *
+ *   cleanup (false): Perform full property cleanup after both
+ *     serialization and deserialization using the delete
+ *     operator. This may cause performance penalties (breaking hidden
+ *     classes in V8) on objects that ResurrectJS touches, to enable
+ *     with care.
+ *
+ *   revive (true): Restore behavior (__proto__) to objects that have
+ *     been resurrected. If this is set to false during serialization,
+ *     resurrection information will not be encoded. You still get
+ *     circularity and Date support.
  *
  * ## Caveats
  *
- *   All constructors must be named and stored in the global variable
- * under than name.
+ *   * All constructors must be named and stored in the global
+ * variable under that name.
  *
- *   The wrapper objects Boolean, String, and Number will be
+ *   * The wrapper objects Boolean, String, and Number will be
  * unwrapped. This means extra properties added to these objects will
  * not be preserved.
  */
