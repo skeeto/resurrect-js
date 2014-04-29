4fccb0cf2bcfa009f55086e660ecc33188adabb3
Christopher Wellons <wellons@nullprogram.com>
2014-04-29 18:33:45 -0400

    Oops, add number check when checking for NaN.

--- resurrect.js
+++ resurrect.js
@@ -400,7 +400,8 @@ Resurrect.prototype.handleAtom = function(atom) {
         return this.builder('RegExp', args);
     } else if (atom === undefined) {
         return this.ref(undefined);
-    } else if (Number.isNaN(atom) || !Number.isFinite(atom)) {
+    } else if (Resurrect.isNumber(atom) &&
+               (Number.isNaN(atom) || !Number.isFinite(atom))) {
         return this.builder('Number', [atom.toString()]);
     } else {
         return atom;
